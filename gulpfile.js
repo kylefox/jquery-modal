4f4341545964529095d79cecd847c046b6d5c470
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-04-26 23:24:36 -0300

    add minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var uglify = require("gulp-uglify");
+var uglifycss = require("gulp-uglifycss");
 var rename = require('gulp-rename');
 var changelog = require('gulp-conventional-changelog');
 
@@ -7,6 +8,13 @@ gulp.task('min', function() {
   gulp.src('jquery.modal.js')
     .pipe(uglify({preserveComments: 'license'}))
     .pipe(rename('jquery.modal.min.js'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('css', function() {
+  gulp.src('jquery.modal.css')
+    .pipe(uglifycss())
+    .pipe(rename('jquery.modal.min.css'))
     .pipe(gulp.dest('.'));
 });
 
